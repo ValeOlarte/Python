9042c97b648538c8c7f7c9e0121715e79276ac27
Valentín Olarte <121908387+ValeOlarte@users.noreply.github.com>
2025-02-27 12:17:43 -0500

    ComputerGues

---
+++ ComputerGuess.py
@@ -0,0 +1,48 @@
+import random
+import time
+
+a = 1
+b = 100
+userNumber = 0
+
+def guessNumber (a, b):
+    
+    intentos = 0
+    print("Este es un programa en el cual pones a adivinar a la maquina el numero elegido ente 1 y 100")
+    
+    while True:
+        
+        userNumber = int(input("Escribe tu numero: "))
+        if 100 >= userNumber >= 1:
+            break
+        else:
+            None
+    
+    while True:
+        
+        intentos = intentos + 1
+        randomNumber = random.randint(a, b)
+        
+        if randomNumber == userNumber:
+            print ("el numero fue: ")
+            print (randomNumber)
+            print ("-La computadora adivino-")
+            print ("adivino en: ", intentos)
+            print(    )
+            break
+            
+        if randomNumber > userNumber:
+            b = randomNumber
+            print ("el numero fue: ")
+            print (randomNumber)
+            print ("-seguir adivinando-")
+            print(    )
+        
+        if randomNumber < userNumber:
+            a = randomNumber
+            print ("el numero fue: ")
+            print (randomNumber)
+            print ("-seguir adivinando-")        
+            print(    )
+            
+guessNumber(a, b)
