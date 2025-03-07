ddd28833e90e9720629bb74463e448cc1708502b
Valentín Olarte <121908387+ValeOlarte@users.noreply.github.com>
2025-03-07 09:17:15 -0500

    Timer

--- Timer.py
+++ Timer.py
@@ -1,2 +1,15 @@
 import time
 
+def timer(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = "{:02d}:{:02d}".format(mins,secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print("timer completed")
+
+t = int(input("Enter the time in seconds: "))
+
+timer(t)
