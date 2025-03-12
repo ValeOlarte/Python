0ae2ef2d450127131f5bc80f7edb35d6594b481c
Valentín Olarte <121908387+ValeOlarte@users.noreply.github.com>
2025-03-12 12:09:41 -0500

    GeneratorPassword 1.2

--- GeneratorPassword.py
+++ GeneratorPassword.py
@@ -1,15 +1,18 @@
 import random
 
 dic = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890+#$%&-*"
+dec = "+#$%&-*1234567890"
 Contraseña = []
 delimiter = ""
 
 dic = list(dic)
+dec = list(dec)
 
 x = 0
 while x < (random.randint(8, 100)):
     x = x + 1
     Contraseña.append(random.choice(dic))
+    Contraseña.append(random.choice(dec))
 
 Contraseña = delimiter.join(Contraseña)
 
